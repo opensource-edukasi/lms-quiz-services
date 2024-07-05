56a5b21a3345726d0251c708f045f654f20a13a9
erikna7 <erikson.saragi@humantechno.co.id>
2024-07-06 02:01:02 +0700

    add Unary Create Quiz

--- internal/domain/quizzes/quiz_repository.go
+++ internal/domain/quizzes/quiz_repository.go
@@ -132,7 +132,7 @@ func (a *QuizRepository) getExtQuestions(ctx context.Context) ([]string, error) 
 func (a *QuizRepository) InsertQuestion(ctx context.Context, question *quizPb.Question) error {
 
 	query := `
-		INSERT INTO question (quiz_id, title, description, storage_id, answer_id, updated_by)
+		INSERT INTO questions (quiz_id, title, description, storage_id, answer_id, updated_by)
 		VALUES ($1, $2, $3, $4, $5, $6)
 		RETURNING id, updated_at, created_at
 		`
@@ -601,6 +601,44 @@ func (a *QuizRepository) InsertQuestionAnswer(ctx context.Context, questionAnswe
 
 	if err != nil {
 		return status.Errorf(codes.Internal, "Exec answer quiz: %v", err)
+	}
+
+	return nil
+}
+
+func (a *QuizRepository) Create(ctx context.Context) error {
+	query := `
+		INSERT INTO quizzes (subject_class_id, topic_subject_id, name, description, end_date, updated_by)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		RETURNING id, created_at, updated_at, updated_by
+	`
+
+	stmt, err := a.tx.PrepareContext(ctx, query)
+	if err != nil {
+		return status.Errorf(codes.Internal, "Prepare statement create quiz: %v", err)
+	}
+	defer stmt.Close()
+
+	a.pb.UpdatedBy = ctx.Value(app.Ctx("user_id")).(string)
+
+	err = stmt.QueryRowContext(ctx,
+		a.pb.SubjectClassId,
+		a.pb.TopicSubjectId,
+		a.pb.Name,
+		a.pb.Description,
+		a.pb.EndDate,
+		a.pb.UpdatedBy,
+	).Scan(&a.pb.Id, &a.pb.CreatedAt, &a.pb.UpdatedAt, &a.pb.UpdatedBy)
+
+	if err != nil {
+		fmt.Println("Error inserting quiz")
+		return status.Errorf(codes.Internal, "Exec create quiz: %v", err)
+	}
+
+	for _, question := range a.pb.Question {
+		if err := a.InsertQuestion(ctx, question); err != nil {
+			return err
+		}
 	}
 
 	return nil
--- internal/domain/quizzes/quiz_service.go
+++ internal/domain/quizzes/quiz_service.go
@@ -126,3 +126,51 @@ func (a *QuizService) Answer(ctx context.Context, in *quizPb.QuizAnswerInput) (*
 
 	return &quizRepo.pbAnswer, nil
 }
+
+func (a *QuizService) Create(ctx context.Context, in *quizPb.QuizCreateInput) (*quizPb.Quiz, error) {
+	var quizRepo QuizRepository
+	var err error
+	quizRepo.tx, err = a.Db.BeginTx(ctx, nil)
+	if err != nil {
+		return &quizRepo.pb, err
+	}
+
+	// TODO: validate quizInput
+	
+	quizRepo.pb = quizPb.Quiz{
+		Description: in.Description,
+		Name:        in.Name,
+		SubjectClassId: in.SubjectClassId,
+		TopicSubjectId: in.TopicSubjectId,
+		EndDate:     in.EndDate,
+	}
+	
+	for _, questionInput := range in.Question {
+		// TODO: validate questionInput
+		question := &quizPb.Question{
+			Title:       questionInput.Title,
+			Description: questionInput.Description,
+			StorageId:   questionInput.StorageId,
+		}
+	
+			for _, opt := range questionInput.Option {
+				// TODO: validate optionInput
+				question.Option = append(question.Option, &quizPb.Option{
+					Description: opt.Description,
+					StorageId:   opt.StorageId,
+				})
+			}
+	
+			quizRepo.pb.Question = append(quizRepo.pb.Question, question)
+		}
+	
+		err = quizRepo.Create(ctx)
+	
+		if err != nil {
+			return &quizRepo.pb, err
+		}
+	
+		quizRepo.tx.Commit()
+	
+		return &quizRepo.pb, nil
+	}
